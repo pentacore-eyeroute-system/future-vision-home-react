8ab663d4c1285565e6493d5afd8fe097ed27cfa6
Gwyenth-Stratpoint <i_gwyenth.lim@stratpoint.com>
2026-04-02 12:41:51 +0800

    feat: add layout shell with theme + routing skeleton

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles.css'
 import App from './App.jsx'
+import { ThemeProvider } from './context/ThemeContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 )
