00a2ca8ffbae17ba7d113d48f64ff74be809b7e8
finalFactorial <ragm923@gmail.com>
2026-06-02 02:03:59 +0800

    chore: change printing from API to url/reviews

--- src/api/reviewApi.js
+++ src/api/reviewApi.js
@@ -7,7 +7,7 @@ const API = axios.create({
   withCredentials: true,
 });
 
-console.log("API: ", API);
+console.log("API: ", `${VITE_API_BASE_URL}/reviews`);
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token') || sessionStorage.getItem('token');
